d300d1e0337e3995f9c5fe3765ac362850636ab6
đức Hoàng <duchoangvo97@gmail.com>
2020-11-03 23:39:02 +0700

    commit

--- GroupProject/DAL/DAL_Bill.cs
+++ GroupProject/DAL/DAL_Bill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,16 @@ namespace GroupProject.DAL
 {
     class DAL_Bill
     {
+        //xem xét lại có nên gôp để xử lý hóa đơn
         DAL_GeneralAccess access = new DAL_GeneralAccess();
+
+        string sql;
+
+        public DataTable LoadBillTable()
+        {
+            sql = "SELECT * FROM Bill";
+            return access.executeQuery(sql);
+        }
 
        
     }
